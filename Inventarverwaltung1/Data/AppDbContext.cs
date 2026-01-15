74ce9ec7d32813e2e95e337adc8d1cda7d24255c
Christian Meng <Christian.meng@outlook.com>
2026-01-15 09:40:58 +0100

    DB anbindung

---
+++ Inventarverwaltung1/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inventarverwaltung1.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Inventargegenstand> Inventargegenstaende => Set<Inventargegenstand>();
+    public DbSet<Abteilung> Abteilungen => Set<Abteilung>();
+    public DbSet<Gruppe> Gruppen => Set<Gruppe>();
+    public DbSet<Fach> Faecher => Set<Fach>();
+    public DbSet<Ort> Orte => Set<Ort>();
+    public DbSet<Zustand> Zustaende => Set<Zustand>();
+    public DbSet<Ausleihe> Ausleihen => Set<Ausleihe>();
+    public DbSet<Benutzer> Benutzer => Set<Benutzer>();
+    public DbSet<Rolle> Rollen => Set<Rolle>();
+}
--- Inventarverwaltung1/Program.cs
+++ Inventarverwaltung1/Program.cs
@@ -1,5 +1,9 @@
 using Inventarverwaltung1.Components;
+using Microsoft.EntityFrameworkCore;
+using Inventarverwaltung1.Data;
+using Microsoft.EntityFrameworkCore;
 using Radzen;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
